772b0b23d648f9281823f8529ad23720a6be80d4
Matheus Henrique <matheush0805@gmail.com>
2024-08-13 20:31:25 -0300

    exercícios

--- aula01/exercicio1/app/index.tsx
+++ aula01/exercicio1/app/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import React from 'react';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
-const image = {uri: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.quizur.com%2Fquiz%2Fqual-shitpost-vc-e-QYAB&psig=AOvVaw2qil5yFGXdSduXBIRaYglO&ust=1723597837262000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKil-Lnk8IcDFQAAAAAdAAAAABAT'};
+const image = {uri: 'https://static.wikia.nocookie.net/onepiece/images/8/87/One_Piece_Anime_Logo.png/revision/latest/scale-to-width-down/1000?cb=20190128234845&path-prefix=pt'};
 
 const App = () => (
   <View style={styles.container}>
