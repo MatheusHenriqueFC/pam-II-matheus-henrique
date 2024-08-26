c2b96b8ce9c9840a2e7765b72b6ff5259db3a471
Matheus Henrique <matheush0805@gmail.com>
2024-08-26 17:43:24 -0300

    aula01

--- aula01/exercicio1/app/index.tsx
+++ aula01/exercicio1/app/index.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { Text, View } from "react-native";
 
-const image = {uri: 'https://static.wikia.nocookie.net/onepiece/images/8/87/One_Piece_Anime_Logo.png/revision/latest/scale-to-width-down/1000?cb=20190128234845&path-prefix=pt'};
-
-const App = () => (
-  <View style={styles.container}>
-  <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-  </ImageBackground>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
-
-export default App;
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
